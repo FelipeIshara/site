199b21002aebd650289ffbf8ef8e0c211289998a
FelipeIshara <63327220+FelipeIshara@users.noreply.github.com>
2025-05-09 15:52:17 +0000

    ajustes

---
+++ power/estilo.js
@@ -0,0 +1,16 @@
+function showTab(tabId) {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+      section.classList.remove('active');
+      section.classList.remove('zoom-in'); 
+    });
+  
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => link.classList.remove('active'));
+  
+    const activeSection = document.getElementById(tabId);
+    activeSection.classList.add('active');
+    activeSection.classList.add('zoom-in'); 
+  
+    document.querySelector(`nav a[onclick="showTab('${tabId}')"]`).classList.add('active');
+  }
